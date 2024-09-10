de5139ccd0afe8d09e8972c849edfa6df432a2da
tomvitello <114203511+tomvitello@users.noreply.github.com>
2024-09-09 23:44:18 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+
+function pesquisar() {
+    
+    let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Busca vazia</p>";
+        return;
+    }
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
+    for (let dado of dados){
+        
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+          resultados += `
+            <div class="item-resultado">
+              <h2>${dado.titulo}</h2>
+              <p class="descricao-meta">
+              ${dado.descricao}
+              </p>
+              <a href="${dado.link}" target="_blank">Mais Informações</a>
+            </div>
+          `
+      }
+    
+        
+    }
+
+    if (!resultados) {
+        resultados = "<p>Informação não encontrada</p>";
+    }
+    
+    section.innerHTML = resultados;
+
+}
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade: A Maior Medalhista Olímpica Brasileira",
+        descricao: "Rebeca Rodrigues de Andrade é uma ginasta artística brasileira que conquistou o coração de milhões de brasileiros com seu talento e determinação. Nascida em Guarulhos, São Paulo, em 1999, Rebeca se tornou um dos maiores nomes do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ouro"
+    },
+    {
+        titulo: "Rayssa Leal: A Fadinha do Skate",
+        descricao: "Rayssa Leal, mais conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Nascida em Imperatriz, Maranhão, em 2008, Rayssa se tornou a skatista mais jovem a participar de uma Olimpíada, aos 13 anos, e conquistou a medalha de prata nos Jogos Olímpicos de Tóquio 2020. Sua história de superação, vindo de uma comunidade carente, inspira milhões de jovens no Brasil e no mundo. ",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada"
+    },
+    {
+        titulo: "Beatriz Souza: A Nova Guarda do Judô Brasileiro",
+        descricao: "Beatriz Souza é uma judoca brasileira que representa a nova geração de atletas do país. Com apenas 26 anos, ela já conquistou a medalha de ouro nas Olimpíadas de Paris 2024, consolidando seu nome no cenário internacional do judô. Sua técnica apurada e determinação a tornaram um dos maiores destaques do esporte brasileiro nos últimos anos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo ouro"
+    }
+];
+
